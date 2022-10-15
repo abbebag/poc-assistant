512164a135be0d7a56c6f225973d619ef5635f67
abbebag <115878554+abbebag@users.noreply.github.com>
2022-10-15 15:11:53 +0200

    refactor: formatting

--- serv.py
+++ serv.py
@@ -1,13 +1,13 @@
 import socket
 
-HOST = '0.0.0.0'
+HOST = "0.0.0.0"
 PORT = 5000
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
 conn, addr = s.accept()
-print('Connected by', addr)
+print("Connected by", addr)
 
 receiving = True
 
@@ -15,10 +15,10 @@ while True:
     if receiving:
         data = conn.recv(1024)
         data = data.decode()
-        #split on newline
-        lines = data.split('\n')
+        # split on newline
+        lines = data.split("\n")
         for line in lines:
-            if line == 'command':
+            if line == "command":
                 receiving = False
             else:
                 print(line)
@@ -26,4 +26,3 @@ while True:
         command = input("Enter command: ")
         conn.send(command.encode())
         receiving = True
-  
